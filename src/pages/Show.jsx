59ef30be8cf983fef39deee559599fb62ea48986
Ashwini Pendkar <ashwinipendkar953@gmail.com>
2023-10-20 11:31:34 +0530

    installed react-query. Replaced useShowById with useQuery custom hook

--- src/pages/Show.jsx
+++ src/pages/Show.jsx
@@ -1,28 +1,13 @@
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 import { getShowById } from '../api/tvmaze';
-
-const useShowById = showId => {
-  const [showData, setShowData] = useState(null);
-  const [showError, setShowError] = useState(null);
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const data = await getShowById(showId);
-        setShowData(data);
-      } catch (error) {
-        setShowError(error);
-      }
-    }
-    fetchData();
-  }, [showId]);
-  return { showData, showError };
-};
+import { useQuery } from '@tanstack/react-query';
 
 const Show = () => {
   const { showId } = useParams();
-  const { showData, showError } = useShowById(showId);
+  const { data: showData, error: showError } = useQuery({
+    queryKey: ['shows', showId],
+    queryFn: () => getShowById(showId),
+  });
 
   if (showError) {
     return <div>We have an error: {showError.meassage}</div>;
